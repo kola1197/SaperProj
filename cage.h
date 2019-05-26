a0dcdc63ae352eaeee48e0dd76b7cc8448280e8e
Gvalhca <kir.diatloff@gmail.com>
2019-05-26 18:50:20 +0300

    rodip kotlej

--- cage.h
+++ cage.h
@@ -1,13 +1,13 @@
 #ifndef CAGE_H
 #define CAGE_H
 #include <iostream>
-
+#include "QString"
 class Cage
 {
 public:
     Cage();
     bool opened = false;
-    std::string text = "0";
+    QString text = "0";
     int countOfMines=0;
     bool mine = false;
     bool mineFlag = false;
